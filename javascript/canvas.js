55d63bb87f288c13ca72687ebd6a49495f0a0e79
James <jamescarclan@yahoo.com>
2020-07-09 21:48:10 +0200

    iteration 2.b

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,18 +1,34 @@
 class HangmanCanvas {
   constructor(secretWord) {
-    this.context = document.getElementById('hangman').getContext('2d');
+    this.context = document.getElementById("hangman").getContext("2d");
     // ... your code goes here
   }
 
   createBoard() {
     // ... your code goes here
-    let newDraw = new HangmanCanvas;
+    let newDraw = new HangmanCanvas();
 
     drawLines();
   }
 
   drawLines() {
-    // ... your code goes here
+    let longestWord = 0;
+    while (longestWord <= secretWord.lenght) {
+      // start the path
+      ctx.beginPath();
+      // starting position is x=50, y=50
+      let move = ctx.moveTo(15, 30);
+      // draw the line that has final coordinates x=250, y=50
+      ctx.lineTo(250, 30);
+
+      // .stroke() executes the drawing
+      ctx.stroke();
+
+      this.context.lineTo(200, 30);
+      // ... your code goes here
+      moveTo(move + 30, 30);
+    }
+    longestWord++;
   }
 
   writeCorrectLetter(letter) {
